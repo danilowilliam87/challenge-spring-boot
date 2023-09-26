5bb94b74c9d01e7174653c0bf19ea4ffa60e1458
danilo <danilowilliam87@outlook.com>
2023-09-26 18:27:22 -0300

    feat: fluxo de exclusao e atualizao OK

--- src/main/java/com/io/blogapi/controller/TopicoController.java
+++ src/main/java/com/io/blogapi/controller/TopicoController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +33,21 @@ public class TopicoController {
     @GetMapping("/listar")
     public ResponseEntity<Page<Topico>> listarTodos(Pageable page){
         return ResponseEntity.ok(this.topicoService.listarTopicos(page));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Topico> findById(@PathVariable Long id){
+         return ResponseEntity.ok(this.topicoService.buscarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Topico> atualizar(@PathVariable Long id,@RequestBody TopicoDto dto){
+       Topico topico = this.topicoService.atualizar(id, dto);
+       return ResponseEntity.ok(topico);
+    } 
+
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id){
+       this.topicoService.excluirTopico(id);
     }
 }
--- src/main/java/com/io/blogapi/service/TopicoService.java
+++ src/main/java/com/io/blogapi/service/TopicoService.java
@@ -22,4 +22,27 @@ public class TopicoService {
     public Page<Topico> listarTopicos(Pageable pageable){
         return this.topicoRepository.findAll(pageable);
     }
+
+    public Topico buscarPorId(Long id){
+        return topicoRepository.findById(id).get();
+    }
+
+    public void excluirTopico(Long id){
+        topicoRepository.deleteById(id);
+    }
+
+
+    public Topico atualizar(Long id, TopicoDto dto){
+        Topico busca = buscarPorId(id);
+        if(busca != null){
+            busca.setAutor(dto.getAutor());
+            busca.setMensagem(dto.getMensagem());
+            busca.setCurso(dto.getCurso());
+            busca.setDataCriacao(dto.getDataCriacao());
+            busca.setStatus(dto.getStatus());
+            busca.setTitulo(dto.getTitulo());
+            return this.topicoRepository.save(busca);
+        }
+        return null;
+    }
 }
